bd3b0cca89da95a754371327bf51f39a84692002
Oliver_localgit <oliver@gedankenfenster.de>
2019-11-06 12:27:12 +0100

    about routes and urls

--- server1.js
+++ server1.js
@@ -6,12 +6,23 @@ const path = require('path')
 var app = express()
 
 
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+
+app.use(express.static("served"))               //   kein '/' hier …
 
 
+app.get("/", function(req, res){
+    
+    res.send("Hallo")
+})
 
 
-
-
+app.listen(port, function(){
+    
+    console.log(`http://localhost:${port}      http://localhost:${port}/test_server1.html      `)
+    
+})
 
 
 
