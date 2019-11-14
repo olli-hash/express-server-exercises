8f698ac7815cd55a790f01a79c07cd01b79aed70
Oliver_localgit <oliver@gedankenfenster.de>
2019-11-14 13:15:56 +0100

    14Nov

--- server2_explore.js
+++ server2_explore.js
@@ -1,123 +1,114 @@
 const port = process.env.PORT || 8080
 const express = require('express')
-// const handlebars = require('express-handlebars')
 const path = require('path')
 var app = express()
-// router = express.Router()
-// app.engine('handlebars', handlebars());
-// app.set('view engine', 'handlebars');
-
 var logger = require('morgan')
 app.use(logger('combined'))
 var cookieParser = require('cookie-parser');
 app.use(cookieParser())
 var bodyParser = require('body-parser');
 
-app.use(express.static("served"))               //   kein '/' hier …
+app.use(express.static("served"))
 app.use("/script",express.static("script")) 
 
-
 app.get("/", function(req, res){
-    
     // Cookies that have not been signed
     console.log('Cookies: ', req.cookies)
-    
     // Cookies that have been signed
     console.log('Signed Cookies: ', req.signedCookies)
-
     res.send("Hallo")
 })
 
-var url_history = []
+var url_history = []                                                        // *global*
 
-app.get(/\/explore_req|\/explore_req\/.*/,   make_handler("req") )
+app.get(/\/explore_req|\/explore_req\/.*/,   make_explorer("req") )
+app.get(/\/explore_res|\/explore_res\/.*/,   make_explorer("res") )
 
-
-function make_handler(o_to_explore) {
+function make_explorer(choice) {
+    switch (choice) {
+        case 'req':
+            var arg_index = 0
+            break
+        case 'res':
+            var arg_index = 1
+            break
+        default:
+            return function(req, res) {  res.send('Wrong choice in make_explorer()')         }
+    }
     
     return the_explorer
-}
-
-
-
-
-
-
-function the_explorer(req,res){
-    
-    var aaa = req.url
-    var aaai = url_history.indexOf(aaa)
-    if (aaai > -1) url_history.splice(aaai,1)
-    url_history.unshift(aaa)
-    
-    
-    var object_path = aaa.split("/")        // eine führender "/" produziert hier ein leeres, erstes Element im Array…
-    object_path.shift()                     // … wird hier entsorgt.
-    if (object_path[object_path.length - 1] === "") object_path.pop()   // falls die URL auf "/" endete, muss auch das letzte 
-                                                                        // Element entsorgt werden.
-    
-    var h1_str = ""
-    var path=[]
-    for (var i = 0; i < object_path.length; i++) {
-        path.push(object_path[i])
-        h1_str += "/<a href='/" + path.join("/") + "'>" + object_path[i] + "</a>"
-    }
-    var str = "<h1 id='objectpath'>" + h1_str + "</h1>"
-    
-    str = str + "<div id='historydisplay'><textarea id='urlhistory'>" +  url_history.join("\n")  +  "</textarea><button id='copy_to_clipboard'>Copy</button><button id='clear_history'>Clear History</button></div>"
-    
-    str += "<br><br><br><br><br>"
-    
-    // Set "leftist" object in object_path auf req and shift on …
-    object_path.shift()
-    var obj = req  
-    
-    function get_last_obj (a) {
+    function the_explorer(req,res){
         
-        if (a.length > 1) { 
-            var aaa = a.shift()
-            obj = obj[aaa]
-            get_last_obj(a)
-        } else if (a.length === 1) {
-            obj = obj[a[0]]
+        var aaa = req.url
+        var aaai = url_history.indexOf(aaa)         // ergibt -1, wenn aaa nicht im *global* Array 'url_history' zu finden ist
+        if (aaai > -1) url_history.splice(aaai,1)   // falls größer: genau dieses Element entfernen
+        url_history.unshift(aaa)                    // Den aktuellen url-String vorne einfügen
+                                                    // (dadurch ergibt sich das typische History-Verhalten, bei dem
+                                                    // doppelte Einträge vermieden werden )
+        
+        var object_path = aaa.split("/")        // eine führender "/" produziert hier ein leeres, erstes Element im Array…
+        object_path.shift()                     // … wird hier entsorgt.
+        if (object_path[object_path.length - 1] === "") object_path.pop()   // falls die URL auf "/" endete, muss auch das letzte 
+                                                                            // Element entsorgt werden.
+        
+        var h1_str = ""
+        var path=[]
+        for (var i = 0; i < object_path.length; i++) {
+            path.push(object_path[i])
+            h1_str += "/<a href='/" + path.join("/") + "'>" + object_path[i] + "</a>"
         }
-    }
-    console.log(object_path)
-    get_last_obj(object_path)
-    
-    
-    
-    var span = "<div class='div_or_span'>"
-    var span_ = "</div>"
-    if (typeof obj === 'object') {
-        for (var i in obj) {
+        var str = "<h1 id='objectpath'>" + h1_str + "</h1>"
+        
+        str += "<div id='historydisplay'><textarea id='urlhistory'>" +  url_history.join("\n")  +  "</textarea><button id='copy_to_clipboard'>Copy</button><button id='clear_history'>Clear History</button></div>"
+        
+        str += "<br><br><br><br><br>"
+        
+        object_path.shift()             // der erste Teil des 'object_path' ist 'explore_req' oder 'explore_res' und kann/muss weg.
+        
+        var obj = arguments[arg_index]  // 'arg_index' von der Mutter-Funktion wählt den Übergabeparameter aus
+                
+        function get_last_obj (a) {     // rekursives Entlang-Hangeln im 'object_path'
             
-            //var i_link = "<a href='" +  i  +  "'>" + i + "</a>"      // ????????????????????????
-            var i_link = "<a href='" +  req.url + "/" +  i  +  "'>" + i + "</a>"     
-            
-            if ( obj[i] && obj[i].toString) {
-                str = str + i_link + ": " + span + obj[i] + span_ + "<br>"
-            } else {
-                str = str + i_link + ": " + span + "----------" + span_ + "<br>"
+            if (a.length > 1) { 
+                var aaa = a.shift()
+                obj = obj[aaa]
+                get_last_obj(a)
+            } else if (a.length === 1) {
+                obj = obj[a[0]]
             }
         }
-    
-    } else {
-        str = str + span + obj + span_ + "<br>"
-    }
-    
-    res.send(template1(req.url) + str + close_template())
+        console.log(object_path)
+        get_last_obj(object_path)
+            
+        var span = "<div class='div_or_span'>"
+        var span_ = "</div>"
+        if (typeof obj === 'object') {
+            for (var i in obj) {
+                
+                //var i_link = "<a href='" +  i  +  "'>" + i + "</a>"      // ????????????????????????
+                var i_link = "<a href='" +  req.url + "/" +  i  +  "'>" + i + "</a>"     
+                
+                if ( obj[i] && obj[i].toString) {
+                    str = str + i_link + ": " + span + obj[i] + span_ + "<br>"
+                } else {
+                    str = str + i_link + ": " + span + "----------" + span_ + "<br>"
+                }
+            }
+            
+        } else {
+            str = str + span + obj + span_ + "<br>"
+        }
+        
+        res.send(template1(req.url) + str + close_template())
+    }    
 }
 
 app.listen(port, function(){
     
-    console.log(`http://localhost:${port}      http://localhost:${port}/produce_GET_POST_AJAX.html      `)
-    console.log(`http://localhost:${port}/explore_req   `)
+    console.log(`http://localhost:${port}     `)
+    console.log(`http://localhost:${port}/explore_req   http://localhost:${port}/explore_res   `)
     
 })
-
-
-
 
 function template1(title){
 var a=`<!doctype html>
